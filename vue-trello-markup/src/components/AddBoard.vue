7fdf37368223c83033f61681f0cfffb1c7f8268b
unknown <usik1234@gmail.com>
2019-12-26 06:47:48 +0900

    added vue-trello-markup

---
+++ vue-trello-markup/src/components/AddBoard.vue
@@ -0,0 +1,32 @@
+<template>
+  <Modal>
+    <div slot="header">
+      <h2>
+        Create new board
+        <a href="" class="modal-default-button" 
+          @click.prevent="SET_IS_ADD_BOARD(false)">&times;</a>
+      </h2>
+    </div>
+    <div slot="body">
+      <form id="add-board-form" 
+        @submit.prevent="addBoard">
+        <input class="form-control" type="text" v-model="input" ref="input">
+      </form>
+    </div>
+    <div slot="footer">
+      <button class="btn" :class="{'btn-success': valid}" type="submit" 
+        form="add-board-form" :disabled="!valid">
+        Create Board</button>
+    </div>
+  </Modal>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-trello-markup/src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <Modal class="modal-card">
+    <div slot="header" class="modal-card-header">
+      <div class="modal-card-header-title">
+        <input class="form-control" type="text" :value="card.title" 
+          :readonly="!toggleTitle" @click="toggleTitle=true" @blur="onBlurTitle"
+          ref="inputTitle">
+      </div>
+      <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
+    </div>
+    <div slot="body">
+      <h3>Description</h3>
+      <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
+        :readonly="!toggleDesc" @click="toggleDesc=true" @blur="onBlurDesc"
+        v-model="card.description" ref="inputDesc"></textarea>
+    </div>
+    <div slot="footer"></div>
+  </Modal>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.modal-card .modal-container {
+  min-width: 300px;
+  max-width: 800px;
+  width: 60%;
+}
+.modal-card-header-title {
+  padding-right: 30px;  
+}
+.modal-close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  font-size: 24px;
+  text-decoration: none;
+}
+.modal-card-header {
+  position: relative;
+}
+</style>
